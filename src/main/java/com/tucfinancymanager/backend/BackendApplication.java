9555105b58626c3f4e1b5caca475151761fe248b
spadaritucks <thiago.spadari02@gmail.com>
2025-08-04 18:22:46 -0300

    Remoção da classe EnvConfig e atualização do método main na classe BackendApplication para eliminar a carga manual de variáveis de ambiente. Adição do arquivo application-prod.properties com configurações de banco de dados e variáveis de ambiente. Atualização do Dockerfile para definir o perfil ativo como 'prod'.

--- src/main/java/com/tucfinancymanager/backend/BackendApplication.java
+++ src/main/java/com/tucfinancymanager/backend/BackendApplication.java
@@ -2,13 +2,11 @@ package com.tucfinancymanager.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import com.tucfinancymanager.backend.config.EnvConfig;
 
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
 	public static void main(String[] args) {
-		EnvConfig.loadEnvironmentVariables();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
